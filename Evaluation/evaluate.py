d2c8b24d60b6f946c39cf3701271f819bbb1b2c0
David Agassi <davidio.agassi@gmail.com>
2018-04-24 07:44:28 +0300

    add mutation duration to experiment

--- Evaluation/evaluate.py
+++ Evaluation/evaluate.py
@@ -8,6 +8,7 @@ threadAgg = [("sizeOpCount", "sizeOps"), ("sizeDuration", "avgSizeDuration")]
 linespecs = ['bo-', 'go-', 'ro-']
 type_legend = ("Unwrapped", "SemiSizable", "Sizable")
 load_type_legend = ("Type: SemiSizable Load: 20i-10d-20s", "Type: Sizable Load: 20i-10d-20s", "Type: SemiSizable Load: 33i-33d-34s", "Type: Sizable Load: 33i-33d-34s", "Type: SemiSizable Load: 45i-45d-10s", "Type: Sizable Load: 45i-45d-10s")
+load_type_legend2 = ("Type: SemiSizable Load: 33i-33d-34s", "Type: Sizable Load: 33i-33d-34s", "Type: SemiSizable Load: 33i-33d-0s", "Type: Sizable Load: 33i-33d-0s")
 algsDict = [
     {
         "name": "SkipList",
@@ -46,12 +47,24 @@ def get_size_time(g):
     inds = ["thread" + str(i) + "avgSizeDuration" for i in range(th_num)]
     return g[inds].mean(axis=1).mean()
 
+
+def get_size_ops(g):
+    th_num = g["nthreads"][0]
+    inds = ["thread" + str(i) + "sizeOps" for i in range(th_num)]
+    return g[inds].mean(axis=1).mean()
+
+
+def get_mutation_time(g):
+    return (10 - get_size_time(g)*get_size_ops(g))*g["nthreads"][0] / g["throughput"].mean()
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "load" or "df" not in store:
         load()
     df = store['df']
     grouped = df.groupby(["name", "ratio", "nthreads", "maxkey"])
     for alg in algsDict:
+        # Wrapper Throughput test
         for alg_type in alg["run_names"]:
             p = [grouped.get_group((alg_type, "50i-50d-0s", th, keynums[-1]))["threadops"].mean()/th for th in threanums]
             plt.plot(threanums, p, marker='o')
@@ -62,6 +75,7 @@ if __name__ == "__main__":
         plt.savefig(os.path.join("plots", alg["name"] + "_wrapper_throughput"))
         plt.close()
 
+        # Size duration test
         for lt in load_types:
             computation_overhead = [get_size_time(grouped.get_group((alg_type, load_types[0], th, keynums[-1]))) for th in threanums]
             for alg_type in alg["run_names"][1:]:
@@ -75,6 +89,20 @@ if __name__ == "__main__":
         plt.savefig(os.path.join("plots", alg["name"] + "_size_duration"))
         plt.close()
 
+        # Mutation duration test
+        for lt in ["33i-33d-34s", "33i-33d-0s"]:
+            for alg_type in alg["run_names"][1:]:
+                p = [1.e6 * get_mutation_time(grouped.get_group((alg_type, lt, th, keynums[-1])))
+                     for th in threanums]
+                plt.plot(threanums, p, marker='o')
+        plt.title(alg["name"] + ": Average mutation Time")
+        plt.ylabel("Average thread ops [uS]")
+        plt.xlabel("Thread number [N]")
+        plt.legend(load_type_legend2, loc='upper left')
+        plt.savefig(os.path.join("plots", alg["name"] + "_mutation_duration"))
+        plt.close()
+
+        # Key range test
         for alg_type in alg["run_names"]:
             p = [grouped.get_group((alg_type, "33i-33d-34s", 16, kr))["threadops"].mean()
                  for kr in keynums]
